415d60849ca112df9efcae4eee966168197d00ec
SnipersCode <validtechrange@gmail.com>
2015-12-19 15:14:27 -0600

    Add container name to script
    Container name was missing from docker inspect

--- run_compose.sh
+++ run_compose.sh
@@ -6,6 +6,6 @@ docker-compose build
 docker-compose up -d
 
 # wait for database, then execute init changes to database
-docker inspect --format '{{ .NetworkSettings.IPAddress }}:27017' | \
+docker inspect --format '{{ .NetworkSettings.IPAddress }}:27017' titdevdocker_database_1 | \
 xargs wget --retry-connrefused --tries=5 -q --wait=1 --spider
 docker exec titdevdocker_database_1 sh -c 'mongo < /scripts/users_add.txt'
