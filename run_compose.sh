bd514c289b687fb553f700c60741d9b999cdb785
SnipersCode <validtechrange@gmail.com>
2015-12-19 15:10:36 -0600

    Wait for db to be up
    Waits for the mongodb to accept connections before adding users

--- run_compose.sh
+++ run_compose.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+
+# build and start docker image
 docker-compose stop
 docker-compose build
 docker-compose up -d
+
+# wait for database, then execute init changes to database
+docker inspect --format '{{ .NetworkSettings.IPAddress }}:27017' | \
+xargs wget --retry-connrefused --tries=5 -q --wait=1 --spider
 docker exec titdevdocker_database_1 sh -c 'mongo < /scripts/users_add.txt'
