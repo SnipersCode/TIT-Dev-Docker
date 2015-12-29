c3fda47485ae4016da72a31101b78124bb520d7d
SnipersCode <validtechrange@gmail.com>
2015-12-29 01:23:19 -0600

    AuthSticky Permissions Unauth
    Add Unauth Mechanism

--- mumo/AuthSticky.py
+++ mumo/AuthSticky.py
@@ -59,6 +59,26 @@ class AuthSticky(MumoModule):
         server.removeUserFromGroup(0, target.session, server_config.not_authed_group)
         server.setState(target)
 
+    def __on_unauth(self, server, action, user, target):
+        try:
+            server_config = getattr(self.cfg(), 'server_%d' % server.id())
+        except AttributeError:
+            server_config = self.cfg().all
+
+        assert action == self.action_auth
+
+        if target.session != user.session:
+            server.sendMessage(user.session, "You cannot unauth other users.")
+            return
+        if any([x == server_config.not_authed_group for x in server.getACL(0)[1]]):
+            server.sendMessage(user.session, "Already Unauthed")
+            return
+
+        # Check site auth
+        server.sendMessage(user.session, "Removed auth for {0}".format(target.userid))
+        server.addUserToGroup(0, user.session, server_config.not_authed_group)
+        server.setState(target)
+
     def userTextMessage(self, server, user, message, current=None): pass
 
     # noinspection PyUnusedLocal
@@ -69,24 +89,30 @@ class AuthSticky(MumoModule):
             server_cfg = self.cfg().all
 
         manager = self.manager()
-        has_authed = False
 
         if user.userid >= 1:
             self.auth_timers[user.userid] = int(time.time())
         server.sendMessage(user.session, "{0} has connected at: {1}".format(
                 user.userid, self.auth_timers.get(user.userid, -1)))
-        if not has_authed:
-            manager.addContextMenuEntry(
-                server,
-                user,
-                self.action_auth,
-                "Auth with Dashboard",
-                self.__on_auth,
-                self.murmur.ContextUser
-            )
-            server.addUserToGroup(0, user.session, server_cfg.not_authed_group)
-            user.channel = server_cfg.not_authed_channel
-            server.setState(user)
+        manager.addContextMenuEntry(
+            server,
+            user,
+            self.action_auth,
+            "Auth with Dashboard",
+            self.__on_auth,
+            self.murmur.ContextUser
+        )
+        manager.addContextMenuEntry(
+            server,
+            user,
+            self.action_auth,
+            "Unauth with Dashboard",
+            self.__on_unauth,
+            self.murmur.ContextUser
+        )
+        server.addUserToGroup(0, user.session, server_cfg.not_authed_group)
+        user.channel = server_cfg.not_authed_channel
+        server.setState(user)
 
     # noinspection PyUnusedLocal
     def userDisconnected(self, server, user, context=None):
@@ -104,7 +130,9 @@ class AuthSticky(MumoModule):
         if user.userid < 1:
             # Lock out if user is not authenticated
             has_authed = False
-        elif user.userid in self.auth_timers and self.auth_timers[user.userid] + svr_cfg.auth_watchdog <= time.time():
+        elif user.userid not in self.auth_timers or (user.userid in self.auth_timers and
+                                                     self.auth_timers[user.userid] +
+                                                     svr_cfg.auth_watchdog <= time.time()):
             # If cache has expired
             self.auth_timers[user.userid] = int(time.time())
             server.sendMessage(user.session, "Renew auth check for {0}".format(user.userid))
@@ -118,7 +146,7 @@ class AuthSticky(MumoModule):
             server.sendMessage(user.session, "In not-authed group, but cache hasn't expired")
             has_authed = False
 
-        if not has_authed:
+        if not has_authed and user.channel != svr_cfg.not_authed_channel:
             server.addUserToGroup(0, user.session, svr_cfg.not_authed_group)
             user.channel = svr_cfg.not_authed_channel
             server.setState(user)
